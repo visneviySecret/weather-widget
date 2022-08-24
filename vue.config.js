7925804ed93741edc1693a18840c3b6524fb19da
visneviySecret <visneviy.secret@yandex.ru>
2022-08-25 02:25:02 +0300

    Config fix

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          'vue-style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
-      }
-    ]
-  },
 })
